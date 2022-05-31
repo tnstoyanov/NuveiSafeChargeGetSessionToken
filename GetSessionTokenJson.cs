614ac601294012cbf372a350e4e0dd4b1779ed2f
tnstoyanov <tnstoyanov@gmail.com>
2022-05-31 15:29:18 +0300

    Add project files.

---
+++ GetSessionTokenJson.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace SafeChargeRest
+
+{
+    class GetSessionTokenChecksumCalc
+    {
+        private static void Main(string[] args)
+        {
+
+            string merchantId = "4914223691328304580";
+            string merchantSiteId = "193578";
+            string clientRequestId = DateTime.Now.ToString("hhmmddMMyy");
+            string timeStamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+            string merchantSecretKey = "e9npgVP9Ot6CEAOu7i3JoVIJcFuZRyl8OOfqaQF675vxtNRXaODVRnngpmaSxdqH";
+            string checksum = ComputeSha256Hash(
+                merchantId
+                + merchantSiteId
+                + clientRequestId
+                + timeStamp
+                + merchantSecretKey
+                );
+            Console.WriteLine("{");
+            Console.WriteLine("\"merchantId\": \"{0}\",", merchantId);
+            Console.WriteLine("\"merchantSiteId\": {0},", merchantSiteId);
+            Console.WriteLine("\"clientRequestId\": \"{0}\",", clientRequestId);
+            Console.WriteLine("\"timeStamp\": \"{0}\",", timeStamp);
+            Console.WriteLine("\"checksum\": \"{0}\"", checksum);
+            Console.WriteLine("}");
+
+            Console.ReadLine();
+
+            string ComputeSha256Hash(string rawData)
+            {
+                // Creates a SHA256   (Works the same way for SHA256)
+                using (SHA256 sha256Hash = SHA256.Create())
+                {
+                    // ComputeHash - returns byte array  
+                    byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                    // Converts byte array to a string   
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < bytes.Length; i++)
+                    {
+                        // x2 for lowercase, X2 for uppercase
+                        builder.Append(bytes[i].ToString("x2"));
+                    }
+                    return builder.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SafeChargeRest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SafeChargeRestApi")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3b2485ec-0e7f-4bee-8cd9-89aafcc6926c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
